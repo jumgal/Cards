d1477caa1d4ed30b71627a476883549cb1c5144a
jumgal <cumgal@gmail.com>
2019-11-28 11:04:39 -0800

    Updates

---
+++ precache-manifest.405e826ff9284bb16b5bf5353aa04fe6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8ff57bc6127b11bec4f62ec63e9b46c",
+    "url": "/Cards/index.html"
+  },
+  {
+    "revision": "f59b309058e7ca6e4072",
+    "url": "/Cards/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8d4696a01f165b51a693",
+    "url": "/Cards/static/js/2.d2f1aafd.chunk.js"
+  },
+  {
+    "revision": "f59b309058e7ca6e4072",
+    "url": "/Cards/static/js/main.5aef0f91.chunk.js"
+  },
+  {
+    "revision": "c4e8f01eecee783bf27d",
+    "url": "/Cards/static/js/runtime-main.2c5500d0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Cards/static/media/logo.25bf045c.svg"
+  }
+]);
