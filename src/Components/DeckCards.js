8317281084e73bf78555849c345eb650c99351ad
jumgal <cumgal@gmail.com>
2019-11-28 14:01:55 -0800

    second commit

---
+++ src/Components/DeckCards.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class DeckCards extends Component {
+
+    render () {
+        return (
+            <h1>This is a Card Component</h1>
+        )
+    }
+}
+
+export default DeckCards;
